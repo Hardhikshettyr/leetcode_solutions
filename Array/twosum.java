024a14173f599454a354443a28e789c38e6de4b3
Hardhikshettyr <hardhikshetty1771@gmail.com>
2026-07-08 11:38:54 +0530

    solution for 1.twosum

--- Array/twosum.java
+++ Array/twosum.java
@@ -1,12 +1,14 @@
 import java.util.*;
 class Solution {
     public int[] twoSumoptimal(int[] nums, int target) {
+        Map<Integer,Integer>new_map=new HashMap<>();
+
         for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
-                    return new int[]{i,j};
-                }
+            int complement=target-nums[i];
+            if(new_map.containsKey(complement)){
+                return new int[]{new_map.get(complement),i};
             }
+            new_map.put(nums[i],i);
         }
         return new int[]{-1,-1};
     }
