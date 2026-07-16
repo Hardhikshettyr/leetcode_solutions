6cc7383547344532c584923a9c61a14c5e8be366
Hardhikshettyr <hardhikshetty1771@gmail.com>
2026-07-16 12:00:05 +0530

    solution for 852.Peak index in a Mountain Array

--- Array/peakindex.java
+++ Array/peakindex.java
@@ -1,3 +1,14 @@
-public class peakindex {
-    
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int low=0, end=arr.length-1;
+        while(low<=end){
+            int mid=(low+end)/2;
+            if(arr[mid]<arr[mid+1]){
+                low=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+        return low;
+    }
 }
