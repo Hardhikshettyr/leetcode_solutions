be8b7d4373f5431c9c0c4a5efd3b2b699a99965f
Hardhikshettyr <hardhikshetty1771@gmail.com>
2026-09-07 10:56:55 +0530

    solution for 152. Maximum Product Subarray

---
+++ Array/maximumproductsubarray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int curmax=nums[0];
+        int curmin=nums[0];
+        int max=nums[0];
+
+        for(int i=1;i<nums.length;i++){
+            int temp=curmax;
+
+            curmax=Math.max(nums[i],Math.max(temp*nums[i],curmin*nums[i]));
+            curmin=Math.min(nums[i],Math.min(temp*nums[i],curmin*nums[i]));
+
+            max=Math.max(curmax,max);
+        }
+        return max;
+    }
+}
